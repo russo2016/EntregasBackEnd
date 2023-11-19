47a760bbde54c41e3f3b20bef72ea0c8b8c2cf7d
russo2016 <russojoaco@gmail.com>
2023-11-19 15:41:23 -0300

    detalle

--- Desafio1.js
+++ Desafio1.js
@@ -44,12 +44,12 @@ const manager = new ProductManager()
 manager.addProduct("Milanesa", "de carne", 2000, "https://upload.wikimedia.org/wikipedia/commons/6/6e/Weekend_in_Buenos_Aires.jpg", "1", 15)
 manager.addProduct("Milanesa", "de pollo, viene con fritas y ensalada", 1800, "https://wildfork.mx/cdn/shop/articles/03_Milanesa_de_Pollo_con_Papas_Fritas.jpg?v=1614571982", "2", 14)
 manager.addProduct("Tarta", "de jamón y queso",1200, "https://lacteosbarraza.com.ar/home/wp-content/uploads/2020/09/tarta-jamon-queso.jpg", "3", 9)
-manager.addProduct("Pizza", "de mozzarella", 1000, "https://img.freepik.com/fotos-premium/pizza-mozzarella-aceitunas-tabla-madera_311379-1163.jpg?w=360", 6)
-manager.addProduct("Hamburguesa", "completa", 1600, "https://media-cdn.tripadvisor.com/media/photo-s/13/2b/b7/fa/hamburguesa-completa.jpg", "2", 20)
+manager.addProduct("Pizza", "de mozzarella", 1000, "https://img.freepik.com/fotos-premium/pizza-mozzarella-aceitunas-tabla-madera_311379-1163.jpg?w=360", 6)//arroja error por campos incompletos
+manager.addProduct("Hamburguesa", "completa", 1600, "https://media-cdn.tripadvisor.com/media/photo-s/13/2b/b7/fa/hamburguesa-completa.jpg", "2", 20)//arroja error por ID duplicado
 
 
 console.log(manager.getProducts())
 
 
 console.log(manager.getProductById(2))
-console.log(manager.getProductById(6))//arroja error por Id inexistente
+console.log(manager.getProductById(6))//arroja error por ID inexistente
