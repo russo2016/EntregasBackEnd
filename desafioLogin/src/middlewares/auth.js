3e70d895df0611fcd88957a9c44a6b41f3246df5
russo2016 <133719171+russo2016@users.noreply.github.com>
2024-01-31 20:03:58 -0300

    Delete desafioLogin directory

--- desafioLogin/src/middlewares/auth.js
+++
@@ -1,7 +0,0 @@
-function auth(req, res, next) {
-    console.log(req.session);
-    if (req.session && req.session.role === "admin") return next();
-    else return res.sendStatus(401);
-}
-  
-export default auth;
--- desafioLogin/src/middlewares/index.js
+++
@@ -1,3 +0,0 @@
-import auth from "./auth.js";
-
-export { auth };
