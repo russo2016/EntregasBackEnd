3d74a954c7bac93c7658f515245b0df86ab93b68
russo2016 <russojoaco@gmail.com>
2024-03-16 13:51:11 -0300

    deleteCartBtn

--- EntregaIntegradora/src/public/js/carts.js
+++ EntregaIntegradora/src/public/js/carts.js
@@ -1,4 +1,5 @@
 const purchaseBtn = document.getElementById('btn-to-purchase');
+const deleteBtn = document.getElementById('btn-delete-cart');
 
 purchaseBtn.addEventListener('click', async (e) => {
     e.preventDefault();
@@ -11,6 +12,31 @@ purchaseBtn.addEventListener('click', async (e) => {
         if (userData.user) {
             const cart = userData.user.cart;
             window.location.href = `/carts/${cart}/purchase`;
+        } else {
+            console.error('No se recibió información de usuario válida');
+        }
+    } catch(error) {
+        console.error('Error al obtener la sesión del usuario:', error);
+    }
+});
+
+deleteBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    try {
+        const response = await fetch('/api/sessions/current');
+        if (!response.ok) {
+            throw new Error('No se pudo obtener la sesión del usuario');
+        }
+        const userData = await response.json();
+        if (userData.user) {
+            const cart = userData.user.cart;
+            const response = await fetch(`/api/carts/${cart}`, {
+                method: 'DELETE'
+            });
+            if (!response.ok) {
+                throw new Error('No se pudo eliminar el carrito');
+            }
+            window.location.href = `/carts/${cart}`;
         } else {
             console.error('No se recibió información de usuario válida');
         }
--- EntregaIntegradora/src/routes/carts.route.js
+++ EntregaIntegradora/src/routes/carts.route.js
@@ -5,9 +5,9 @@ import auth from "../middlewares/auth.js";
 const router = Router();
 
 router.get("/:id",auth("PUBLIC"), controller.getCartById);
-router.post("/:cid/products/:pid",auth("user"), controller.addProductToCart);
-router.put("/:cid/product/:pid",auth("user"), controller.updateProductInCart);
-router.delete("/:id",auth("admin"), controller.deleteCartById);
+router.post("/:cid/products/:pid",auth("PUBLIC"), controller.addProductToCart);
+router.put("/:cid/product/:pid",auth("PUBLIC"), controller.updateProductInCart);
+router.delete("/:id",auth("PUBLIC"), controller.deleteCartById);
 router.delete("/:cid/product/:pid",auth("user"), controller.removeProductFromCart);
 
 
