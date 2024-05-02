d0a496c594a6e75c1c95c94f6f5c94c4024d7ca6
russo2016 <russojoaco@gmail.com>
2024-05-02 11:57:06 -0300

    agregado delete product

--- EntregaIntegradora/src/public/js/carts.js
+++ EntregaIntegradora/src/public/js/carts.js
@@ -1,5 +1,6 @@
 const purchaseBtn = document.getElementById('btn-to-purchase');
 const deleteBtn = document.getElementById('btn-delete-cart');
+const deleteProductFromCart = document.getElementById('delete-product-from-cart');
 
 purchaseBtn.addEventListener('click', async (e) => {
     e.preventDefault();
@@ -37,6 +38,32 @@ deleteBtn.addEventListener('click', async (e) => {
                 throw new Error('No se pudo eliminar el carrito');
             }
             window.location.reload();
+        } else {
+            console.error('No se recibió información de usuario válida');
+        }
+    } catch(error) {
+        console.error('Error al obtener la sesión del usuario:', error);
+    }
+});
+
+deleteProductFromCart.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const productId = document.getElementById("productId").value
+    try {
+        const response = await fetch('/api/sessions/current');
+        if (!response.ok) {
+            throw new Error('No se pudo obtener la sesión del usuario');
+        }
+        const userData = await response.json();
+        if (userData.user) {
+            const cart = userData.user.cart;
+            const response = await fetch(`/api/carts/${cart}/products/${productId}`, {
+                method: 'DELETE'
+            });
+            if (!response.ok) {
+                throw new Error('No se pudo eliminar el producto del carrito');
+            }
+            //window.location.reload();
         } else {
             console.error('No se recibió información de usuario válida');
         }
